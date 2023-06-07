a338eece9618c7e3b620654f0353f1286e6c37b1
Yujin Shin <dbwls020906@gmail.com>
2023-06-07 14:29:05 +0900

    tree_traversal

--- tree traversal/tree traversal/tree traversal.c
+++ tree traversal/tree traversal/tree traversal.c
@@ -5,14 +5,14 @@
 트리 순회
 */
 
-typedef struct tree
+typedef struct tree // 트리 구조체 선언
 {
 	int value;
 	struct tree* left;
 	struct tree* right;
 }tree;
 
-tree* setBinTree(int* arr, int size, int index)
+tree* setBinTree(int* arr, int size, int index) // 이진트리 구현하기
 {
 	int leftIdx, rightIdx;
 	tree* newNode;
@@ -81,7 +81,7 @@ int main()
 	int arr[] = { 6,4,8,2,5,7,9,1,3 };
 
 	tree* root = NULL;
-	root = setBinTree(arr, 9, 0);
+	root = setBinTree(arr, 9, 0); // arr과 arr의 size(size = 9), index = 0
 
 	printf("tree preorder : ");
 	displayPreOrder(root);
